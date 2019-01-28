4d2d4258001b2707eae9a3f2e987addd5f01e1a4
kasper023 <askar.baygenzhin@gmail.com>
2019-01-28 12:22:13 +0600

    First

---
+++ Week1/Task 1/Task 1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine()); // Ввод кол-ва чисел
+            List<int> ans = new List<int>(); // Создание "вектора" для хранения ответа
+            string[] arr = Console.ReadLine().Split(); // Создание и ввод чисел через пробел
+            for (int i = 0; i < N; i++) // Создание цикла для проверки каждого из чисел на простоту
+            {
+                int a = int.Parse(arr[i]); 
+                int ok = 0;
+                for (int j = (int)(Math.Sqrt(a)); j > 1 && ok == 0; j--)
+                    if (a % j == 0)
+                        ok = 1;
+                if (ok == 0 && a != 1) // Вынужденно создаем условие для 1
+                    ans.Add(a);
+            }
+            Console.WriteLine(ans.Count); // Вывод кол-ва простых чисел
+            for (int i = 0; i < ans.Count; i++) 
+                Console.Write(ans[i] + " "); // С помощью цикла вывод все простые числа через пробел
+            Console.ReadKey(); // некий "system("pause")
+        }
+    }
+}
---
+++ Week1/Task 3/Task 3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine()); // Создание и введение кол-ва чисел
+            List<int> ans = new List<int>(); // Создание вектора для хранения ответа
+            string[] arr = Console.ReadLine().Split(); // Создание и введение чисел через пробел
+            for (int i = 0; i < N; i++) // Обход всех чисел
+            {
+                int a = int.Parse(arr[i]);
+                ans.Add(a); // Двойное добавление чисел для их повторения
+                ans.Add(a);
+            }
+
+            for (int i = 0; i < ans.Count; i++) // Вывод ответа через пробел
+                Console.Write(ans[i] + " ");
+            Console.ReadKey(); // system("pause")
+        }
+    }
+}
