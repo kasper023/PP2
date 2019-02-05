06a698edda42147039471877f73dd9cfc5f8af14
kasper023 <askar.baygenzhin@gmail.com>
2019-02-05 18:05:16 +0600

    second chance

---
+++ Week2/Task 2/Task 2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("input.txt");
+            string[] s = sr.ReadToEnd().Split( );
+            sr.Close();
+            List<int> ans = new List<int>();
+            for ( int it = 0; it < s.Count(); it++)
+            {
+                int a = int.Parse(s[it]);
+                int prime = 1;
+                for (int i = 2; i <= (int)Math.Sqrt(a) && prime == 1; i++)
+                    if (a % i == 0)
+                        prime = 0;
+                if (a != 1 && prime == 1)
+                    ans.Add(a);
+            }
+            StreamWriter sw = new StreamWriter("output.txt");
+            for (int i = 0; i < ans.Count(); i++)
+                sw.Write(ans[i] + " ");
+            sw.Close();
+            Console.ReadKey();
+        }
+    }   
+}
---
+++ Week2/Task1/Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string t = s;
+            bool check = true;
+            for( int i = 0; i < s.Length && check == true; i++)
+                if (t[s.Length - i - 1] != s[i])
+                    check = false;
+            if (check == true)
+                Console.WriteLine("Yes");
+            else
+                Console.WriteLine("No");
+            Console.ReadKey();
+        }
+    }
+}
